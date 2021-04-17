b6809aa5e3a9d7e7f2ed882149b20a96c1f9114f
alearg <ale150375@hotmail.com>
2021-04-17 19:02:12 -0300

    arregle api

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,8 @@
 const API = process.env.API;
 
 const getData = async(id) => {
-    const apiURl = id ? `${API}${id}` : API;
+    //const apiURl = id ? `${API}${id}` : API;
+    const apiURl = 'https://randomuser.me/api/'
     try {
         const response = await fetch(apiURl);
         const data = await response.json();
